96a108f66608038f4bda848235cdce8293860f83
Tilak Thummanapally <82318125+Tilak0205@users.noreply.github.com>
2023-05-27 14:27:06 +0530

    Update views.py

--- calendar_integration/views.py
+++ calendar_integration/views.py
@@ -26,31 +26,6 @@ class GoogleCalendarInitView(View):
         # return redirect(auth_url)
         return redirect(auth_url)
 
-# class GoogleCalendarRedirectView(View):
-#     def get(self, request):
-#         code = request.GET.get('code')
-
-#         flow = InstalledAppFlow.from_client_secrets_file(
-#             os.path.join(settings.BASE_DIR, 'calendar_integration/client_secrets.json'),
-#             scopes=['https://www.googleapis.com/auth/calendar.readonly',
-#             'https://www.googleapis.com/auth/calendar',
-#           'https://www.googleapis.com/auth/userinfo.email',
-#           'https://www.googleapis.com/auth/userinfo.profile',
-#           'openid'],
-#         )
-#         flow.redirect_uri = settings.GOOGLE_REDIRECT_URI
-
-#         flow.fetch_token(code=code)
-
-#         credentials = flow.credentials
-#         service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
-
-#         events = service.events().list(calendarId='primary').execute()
-#         # Handle events data as per your requirements
-
-#         return HttpResponse('Calendar events retrieved successfully!')
-
-
 class GoogleCalendarRedirectView(View):
     def get(self, request):
         code = request.GET.get('code')
